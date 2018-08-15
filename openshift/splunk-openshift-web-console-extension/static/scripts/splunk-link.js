678f23c3e44d2eee9ac935da07f641ffb22488b3
Mattia Mascia <mmascia@redhat.com>
2018-08-15 11:04:15 +0200

    splunk properties

--- openshift/splunk-openshift-web-console-extension/static/scripts/splunk-link.js
+++ openshift/splunk-openshift-web-console-extension/static/scripts/splunk-link.js
@@ -1,15 +1,28 @@
 (function () {
-    // Splunk Link
-
     'use strict';
-    angular.module("splunk-extension", ['openshiftConsole'])
+
+
+    angular.module("extension.splunk", ['openshiftConsole'])
         .run(function (extensionRegistry) {
             extensionRegistry.add('log-links', _.spread(function (resource, options) {
-                return {
-                    type: 'dom',
-                    node: '<span><a href="https://splunk">' + resource.metadata.name + '</a><span class="action-divider">|</span></span>'
-                };
+                return splunkLink(resource, options);
             }));
         });
-    hawtioPluginLoader.addModule("splunk-extension");
+
+    function splunkLink(resource, options) {
+        var splunk = {
+            url: window.OPENSHIFT_EXTENSION_PROPERTIES.splunkURL,
+            prefix: window.OPENSHIFT_EXTENSION_PROPERTIES.splunkQueryPrefix,
+            postfix: window.OPENSHIFT_EXTENSION_PROPERTIES.splunkQueryPostfix
+        };
+
+        return {
+            type: 'dom',
+            template: '<span><a href="' + splunk.url + '">' + resource.metadata.name + '</a><span class="action-divider">|</span></span>'
+        };
+    }
+
+
+    hawtioPluginLoader.addModule("extension.splunk");
+
 }());
